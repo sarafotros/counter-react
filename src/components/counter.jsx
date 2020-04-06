db3c3d94f32048f698497274e535de773da9cae0
sarafotros <sarafotros@gmail.com>
2020-04-06 12:36:33 +0100

    First commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+	state = {
+        count: 0
+    };
+    
+    handleIncrement = () => {
+       this.setState({ count: this.state.count + 1 })
+    }
+   
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? 'Zero' : count;
+	}
+	getBadge() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
+	}
+
+    render() {
+        
+        // let classes = this.getBadge();
+		return (
+			<React.Fragment>
+				<span className={this.getBadge()}>
+					{' '}
+					{this.formatCount()}
+				</span>
+				<button onClick={() => this.handleIncrement({ id: 1 })} className="btn btn-secondary btn-sm">Increment</button>
+				<button onClick={this.handleDelete} className="btn btn-danger btn-sm-m2">Delete</button>
+               
+			</React.Fragment>
+		);
+	}
+
+ 
+}
+ 
+export default Counter;
