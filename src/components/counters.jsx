1d494347bc8d9896f44034102fefb104cca9097f
sarafotros <sarafotros@gmail.com>
2020-04-08 23:47:24 +0100

    nav bar first steps

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,44 +3,16 @@ import Counter from './counter';
 
 
 class Counters extends Component {
-    state = { 
-        counters: [
-            { id: 1, value: 0 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 },
-        ]
-    }
-
-    handleIncrement = counter => {
-        const counters = [...this.state.counters]
-        const index = counters.indexOf(counter)
-        counters[index] = { ...counter }
-        counters[index].value++;
-        this.setState({ counters})
-    }
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c
-        })
-        this.setState({ counters})
-    }
-    
-    handleDelete = counterId => {
-        const counters = this.state.counters.filter(c => c.id !== counterId) 
-        this.setState ({ counters})
-    }
+ 
     render() { 
         return (  
             <div>
-                <button className="btn btn-primary btn-sm m-2" onClick={this.handleReset}>Reset</button>
-                {this.state.counters.map(counter => (<Counter
+                <button className="btn btn-primary btn-sm m-2" onClick={this.props.onReset}>Reset</button>
+                {this.props.counters.map(counter => (<Counter
                     key={counter.id}
                     counter={counter} 
-                    onDelete={this.handleDelete}
-                    onIncrement={this.handleIncrement}
+                    onDelete={this.props.onDelete}
+                    onIncrement={this.props.onIncrement}
                     />))}
             </div>
         );
